f302bf11c15129d97b424086f36bc0a6d2dde2c6
bmaupin <bmaupin@users.noreply.github.com>
2020-02-11 12:49:00 -0500

    Fall back to built-in copy functionality for multiple selections
    vscode doesn't seem to copy the formatting when there are multiple selections/cursors
    
    Closes #1

--- src/extension.ts
+++ src/extension.ts
@@ -9,16 +9,16 @@ export function activate(context: vscode.ExtensionContext) {
         const document = editor.document;
         if (!document) return;
 
-        const selectedTexts = editor.selections.map((selection) => document.getText(selection));
-        if (!selectedTexts) return;
+        if (editor.selections.length === 1) {
+            const selectedText = document.getText(editor.selection);
+            if (!selectedText) return;
 
-        // I don't see any simple way in the API to get the current EOL...
-        let eol = '\n';
-        if (document.eol === vscode.EndOfLine.CRLF) {
-            eol = '\r\n';
+            await vscode.env.clipboard.writeText(selectedText);
+        } else {
+            // When there are multiple selections (due to multiple cursors), vscode doesn't seem to copy the formatting.
+            // So just cheat and use the default built-in copy functionality.
+            vscode.commands.executeCommand('editor.action.clipboardCopyAction');
         }
-
-        await vscode.env.clipboard.writeText(selectedTexts.join(eol));
     });
 
     context.subscriptions.push(disposable);
