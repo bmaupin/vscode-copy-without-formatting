db5ef69ad16df9a42bef43e64b24b27fcbbc2180
bmaupin <bmaupin@users.noreply.github.com>
2020-02-11 12:56:59 -0500

    Reformat extension.ts with prettier
    And move a couple lines to a better spot

--- src/extension.ts
+++ src/extension.ts
@@ -2,27 +2,29 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.copyWithoutFormatting', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return;
+  let disposable = vscode.commands.registerCommand(
+    'extension.copyWithoutFormatting',
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) return;
 
+      if (editor.selections.length === 1) {
         const document = editor.document;
         if (!document) return;
 
-        if (editor.selections.length === 1) {
-            const selectedText = document.getText(editor.selection);
-            if (!selectedText) return;
+        const selectedText = document.getText(editor.selection);
+        if (!selectedText) return;
 
-            await vscode.env.clipboard.writeText(selectedText);
-        } else {
-            // When there are multiple selections (due to multiple cursors), vscode doesn't seem to copy the formatting.
-            // So just cheat and use the default built-in copy functionality.
-            vscode.commands.executeCommand('editor.action.clipboardCopyAction');
-        }
-    });
+        await vscode.env.clipboard.writeText(selectedText);
+      } else {
+        // When there are multiple selections (due to multiple cursors), vscode doesn't seem to copy the formatting.
+        // So just cheat and use the default built-in copy functionality.
+        vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+      }
+    }
+  );
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
